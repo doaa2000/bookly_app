83c359900d83c899223181cf40222e51372e121d
doaa2000 <da2000710@gmail.com>
2023-05-06 18:18:09 +0300

    create home repo method

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/faliure.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  Future<Either<Faluire, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Faluire, List<BookModel>>> fetchFeaturedBooks();
 }
